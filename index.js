ab69de894a02743948e76d8cca239c78a9521f06
Awais Ali <awaisali786378@gmail.com>
2025-09-10 18:11:28 +0500

    Handle errors in the utils folder

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import express from "express";
 dotenv.config();
 
-connectDB();
+const app = express();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`SERVER is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO_DB Connection failed");
+  });
